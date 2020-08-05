6d61e669c2a4e6a670adcb2e43101acf76442831
mldn <mldn@163.com>
2020-08-05 14:42:36 +0800

    Change Files

--- Demo.java
+++ Demo.java
@@ -1,5 +1,3 @@
-public class Hello{
-	public static void main(String args[]){
-		System.out.println("hello git!!!");
-	}
+public class Demo implements java.io.Serializable{
+	private Integer empno;
 }
