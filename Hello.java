9e72edef220f28c5f3bb7cf06052f3409b173a8e
mldn <mldn@163.com>
2020-08-13 00:39:41 +0800

    Change Hello.java in dev branch

--- Hello.java
+++ Hello.java
@@ -1,9 +1,6 @@
+﻿import java.util.*
 public class Hello{
 	public static void main(String args[]){
-		//System.out.println("增加注释");
-		System.out.println("增加1");
-		System.out.println("增加2");
-		System.out.println("test");
-		System.out.println("test1");
+		Date date = new Date[];
 	}
 }
