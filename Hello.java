3655b1b00cc9f6ec54f50c7687c7daf93ace96c6
mldn <mldn@163.com>
2020-08-13 00:41:15 +0800

    Change Hello.java in master branch

--- Hello.java
+++ Hello.java
@@ -1,9 +1,6 @@
-public class Hello{
+﻿public class Hello{
 	public static void main(String args[]){
-		//System.out.println("增加注释");
-		System.out.println("增加1");
-		System.out.println("增加2");
-		System.out.println("test");
+		
 		System.out.println("test1");
 	}
 }
